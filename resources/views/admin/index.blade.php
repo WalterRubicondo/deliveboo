a851487879c768c44e05549b76e69345f027c532
Walter <walterrubicondo@gmail.com>
2021-06-16 15:40:08 +0200

    Robe

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,6 +7,8 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
+                <a href="{{route('admin.restaurants.index')}}">Go to Rest</a>
+
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
