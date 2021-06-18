8bceacd215a7f4aaae5811688f2f60bdf0e5c9c5
giuseppe <placidagiuseppe@gmail.com>
2021-06-18 12:37:31 +0200

    pusha andreaaaa

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -30,20 +30,16 @@
             </div>
 
             <div class="form-group">
-              <label for="open_hour">Open Hour</label>
-              <input class="form-control @error('open_hour') is-invalid @enderror" id="open_hour" type="time" name="open_hour" value="{{ old('open_hour') }}">
-              @error('open_hour')
+              <label for="tag">Category</label>
+              <select class="form-control @error('genre_ids') is-invalid @enderror" id="genre" name="genre_ids[]" multiple>
+              @foreach($genres as $genre)
+                <option value="{{$genre->id}}">{{$genre->name}}</option>
+              @endforeach
+              </select>
+              @error('genre_ids')
                 <small class="text-danger">{{ $message }}</small>
               @enderror
-            </div>
-
-            <div class="form-group">
-              <label for="close_hour">Close Hour</label>
-              <input class="form-control @error('close_hour') is-invalid @enderror" id="close_hour" type="time" name="close_hour" value="{{ old('close_hour') }}">
-              @error('close_hour')
-                <small class="text-danger">{{ $message }}</small>
-              @enderror
-            </div>
+             </div>
 
             <div class="form-group">
               <label for="restaurant_address">Restaurant Address</label>
